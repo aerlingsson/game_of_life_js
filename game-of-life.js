e75f469afc0e6c1957801d9e9c2bee7fe6c22be9
aerlingsson <arnar.h.erlingsson@gmail.com>
2025-02-01 21:06:45 +0100

    Add reset button and click to toggle live

--- game-of-life.js
+++ game-of-life.js
@@ -3,6 +3,7 @@ const WIDTH = 1024
 const CELL_SIZE = 32
 const CELL_LIVE_COLOR = "lime"
 const TICK_TIME = 100
+const RANDOM_THRESHOLD = 0.9
 
 const BACKGROUND_COLOR = "#1e1f1e";
 
@@ -13,36 +14,40 @@ if ((HEIGHT % CELL_SIZE) != 0 || (WIDTH % CELL_SIZE) != 0)
 
 let started = false;
 const startStopButton = document.getElementById("startButton")
+const resetButton = document.getElementById("resetButton")
+const randomButton = document.getElementById("randomButton")
+
+const canvas = document.getElementById("canvas")
+const canvasRect = canvas.getBoundingClientRect()
+
+const canvasCtx = canvas.getContext("2d")
+canvasCtx.canvas.height = HEIGHT
+canvasCtx.canvas.width = WIDTH
 
 function flipStartButton() {
     started = !started
     startStopButton.innerText = started ? "Stop" : "Start"
 }
 
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
-ctx.canvas.height = HEIGHT
-ctx.canvas.width = WIDTH
-
 let cells = [[]]
 
 function drawCell(c) {
-    ctx.strokeStyle = "black"
-    ctx.strokeRect(c.x, c.y, CELL_SIZE, CELL_SIZE)
-    ctx.fillStyle = c.live ? CELL_LIVE_COLOR : BACKGROUND_COLOR
-    ctx.fillRect(c.x, c.y, CELL_SIZE, CELL_SIZE)
+    canvasCtx.strokeStyle = "black"
+    canvasCtx.strokeRect(c.x, c.y, CELL_SIZE, CELL_SIZE)
+    canvasCtx.fillStyle = c.live ? CELL_LIVE_COLOR : BACKGROUND_COLOR
+    canvasCtx.fillRect(c.x, c.y, CELL_SIZE, CELL_SIZE)
 }
 
 function drawCells() {
     cells.forEach(row => row.forEach(drawCell))
 }
 
-function setup() {
+function setup(random) {
     for (let x = 0; x <= WIDTH / CELL_SIZE; x++) {
         const row = []
 
         for (let y = 0; y <= HEIGHT / CELL_SIZE; y++)
-            row.push({ x: x * CELL_SIZE, y: y * CELL_SIZE, live: Math.random() > 0.90 })
+            row.push({ x: x * CELL_SIZE, y: y * CELL_SIZE, live: random ? Math.random() > RANDOM_THRESHOLD : false })
 
         cells.push(row)
     }
@@ -73,26 +78,23 @@ function liveNeighbors(x, y) {
     return neighbors.filter((n) => n.live).length
 }
 
-startStopButton.addEventListener("click", flipStartButton)
-
-document.getElementById("resetButton").addEventListener("click", () => {
-    if (started)
-        flipStartButton()
-    setup()
-})
-
 function tick() {
     if (started) {
         cells.forEach((row, x) => {
             row.forEach((cell, y) => {
                 const liveNeighborsCount = liveNeighbors(x, y)
 
+                // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
+                // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
                 if (liveNeighborsCount < 2)
                     cell.live = false
+                // Any live cell with two or three live neighbours lives on to the next generation.
                 if (liveNeighborsCount === 2 || liveNeighborsCount === 3)
                     cell.live = cell.live
+                // Any live cell with more than three live neighbours dies, as if by overpopulation.
                 if (liveNeighborsCount > 3)
                     cell.live = false
+                // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                 if (liveNeighborsCount === 3 && !cell.live)
                     cell.live = true
             })
@@ -104,8 +106,38 @@ function tick() {
     setTimeout(tick, TICK_TIME)
 }
 
+startStopButton.addEventListener("click", flipStartButton)
+
+resetButton.addEventListener("click", () => setup(false))
+
+randomButton.addEventListener("click", () => {
+    if (started)
+        flipStartButton()
+    setup(true)
+})
+
 console.log("Init...")
-setup()
+setup(true)
+
+canvas.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    const x = event.offsetX
+    const y = event.offsetY
+
+    cells.forEach(row => {
+        row.forEach(cell => {
+            if (x >= cell.x && x < cell.x + CELL_SIZE && y >= cell.y && y < cell.y + CELL_SIZE) {
+                console.log(cell)
+                cell.live = !cell.live
+                drawCells()
+            }
+        })
+    })
+
+    console.log("Click", x, y)
+})
+
 drawCells()
 console.log("Done")
 tick()
